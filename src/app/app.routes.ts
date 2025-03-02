7021973b62518cc8d0521512637da63895fee5d8
Nknky09 <ks.shido@gmail.com>
2025-03-02 15:56:58 -0500

    update css on product detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
-import { ReactHostComponent } from '../react-host/react-host.component';
-import { reactMatcher } from './react-matcher';
+// import { ReactHostComponent } from '../react-host/react-host.component';
+// import { reactMatcher } from './react-matcher';
 
 export const routes: Routes = [
   { path: 'products', component: ProductListComponent },
   { path: 'products/:id', component: ProductDetailComponent },
-  { matcher: reactMatcher, component: ReactHostComponent },
+  // { matcher: reactMatcher, component: ReactHostComponent },
   { path: '', redirectTo: '/products', pathMatch: 'full' },
   { path: '**', redirectTo: '/products' },
 ];
