5e3078c0c6ba0048da0fa9e12355c8b7e8df7dd5
Nknky09 <ks.shido@gmail.com>
2025-03-02 15:47:14 -0500

    update code for product list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import {
+  Product,
+  ProductCardComponent,
+} from '../product-card/product-card.component';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  standalone: true,
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
+  imports: [ProductCardComponent, CommonModule],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
+
+  onProductSelected(product: Product): void {
+    this.router.navigate(['/products', product.id]);
+  }
 }
