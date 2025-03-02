a692451f35eb8b2da6b5e542561f7fdb218d7310
Nknky09 <ks.shido@gmail.com>
2025-03-02 15:45:23 -0500

    product service, add mock data and fetch

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Product } from '../components/product-card/product-card.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      price: 10.0,
+      description: 'Description of Product 1',
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      price: 20.0,
+      description: 'Description of Product 2',
+    },
+    {
+      id: 3,
+      name: 'Product 3',
+      price: 30.0,
+      description: 'Description of Product 3',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  getProducts(): Observable<Product[]> {
+    return of(this.products);
+  }
+
+  getProductById(id: number): Observable<Product | undefined> {
+    const product = this.products.find((p) => p.id === id);
+    return of(product);
+  }
 }
